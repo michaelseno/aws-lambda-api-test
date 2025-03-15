c91c0000fbbfdbbf3a904b5bd4077aba3ef3d017
Michael Jordan Seno <mjseno@Michaels-MacBook-Pro-2.local>
2025-03-15 21:26:48 +0800

    updated the notification_service.py by adding timestamp

--- src/notification_service.py
+++ src/notification_service.py
@@ -3,17 +3,24 @@ import json
 import boto3
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 
 class NotificationService:
     def __init__(self):
         load_dotenv()
-        self.sns_client = boto3.client("sns", region_name='us-west-2')
+        self.sns_client = boto3.client("sns",
+                                       region_name=os.getenv("AWS_REGION"))
         self.sns_topic_arn = os.getenv("SNS_TOPIC_ARN")
 
     def publish(self, subject, message):
         self.sns_client.publish(
             TopicArn=self.sns_topic_arn,
             Message=json.dumps(message),
-            Subject=subject
+            Subject=f"{subject} - {self.parse_date(date=datetime.now())}"
         )
+
+    @staticmethod
+    def parse_date(date):
+        return date.strftime("%Y-%m-%d %H:%M:%S")
+
