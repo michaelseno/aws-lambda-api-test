2235d90b48d132999e989f25a8ee422e234db38d
Michael Jordan Seno <mjseno@Michaels-MacBook-Pro-2.local>
2025-03-15 20:59:59 +0800

    added region name in sns_client

--- src/notification_service.py
+++ src/notification_service.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 class NotificationService:
     def __init__(self):
         load_dotenv()
-        self.sns_client = boto3.client("sns")
+        self.sns_client = boto3.client("sns", region_name='us-west-2')
         self.sns_topic_arn = os.getenv("SNS_TOPIC_ARN")
 
     def publish(self, subject, message):
